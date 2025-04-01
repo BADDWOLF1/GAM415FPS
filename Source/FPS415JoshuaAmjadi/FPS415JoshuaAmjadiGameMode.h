093b4140f393cfd7f9bdc0930ff8a47a1026dd3d
BADDWOLF1 <joshua.amjadi@snhu.edu>
2025-03-31 23:50:44 -0700

    Add files via upload

---
+++ Source/FPS415JoshuaAmjadi/FPS415JoshuaAmjadiGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "FPS415JoshuaAmjadiGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AFPS415JoshuaAmjadiGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AFPS415JoshuaAmjadiGameMode();
+};
+
+
+
